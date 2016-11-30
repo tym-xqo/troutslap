037331701cd8382a390e85ed333c389055c1ddfa
Henry Rizzi <henry.rizzi@adops.com>
2016-11-30 13:38:12 -0600

    ability to run locally

--- troutslap.py
+++ troutslap.py
@@ -73,5 +73,5 @@ def user_id_from_name(user):
     i = users.info(user_id)
     return i['user']['id']
 
-# if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
