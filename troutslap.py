aa479b2c1d38d00e653b2e01e3d61a62501978cf
Henry Rizzi <henry.rizzi@adops.com>
2016-11-30 13:27:16 -0600

    updates and dockerization

--- troutslap.py
+++ troutslap.py
@@ -10,36 +10,62 @@ app = Flask(__name__)
 slack = Slack(app)
 app.add_url_rule('/', view_func=slack.dispatch)
 
+# On GCE, we have files mounted at /secret for
+# incoming webhook URL and team token
+# Or you can hard-code values in the except blocks below.
+# try:
+#     with open('/secret/hookurl', 'r') as hookf:
+#         url = hookf.read().strip()
+# except:
+#     url = ("https://hooks.slack.com/services/"
+#            "T02594HP0/B081REU01/PjOvu5UAGNgVKUTydc3GqS6L")  # <- fake ;)
+# try:
+#     with open('/secret/token', 'r') as tokenf:
+#         valid = tokenf.read().strip()
+# except:
+#     valid = "bZKQqL4qkCOORlwzJRAPAvNc"  # phony
+# try:
+#     with open('/secret/teamid', 'r') as teamf:
+#         team = teamf.read().strip()
+# except:
+#     team = "T02594HP0"  # phony
+# try:
+#     with open('/secret/slacktoken', 'r') as slacktokenf:
+#         slacktoken = slacktokenf.read().strip()
+# except:
+#     slacktoken = "bZKQqL4qkCOORlwKQqzJRAPORlwzZKQqL4qkAvNc"
+
 url = os.getenv('HOOKURL')
+valid = os.getenv('TOKEN')
+team = os.getenv('TEAM')
 slacktoken = os.getenv('SLACKTOKEN')
 
 
-@slack.command('slap', token=os.getenv('TOKEN'),
-               team_id=os.getenv('TEAM'), methods=['POST'])
+@slack.command('slap', token=valid,
+               team_id=team, methods=['POST'])
 def slap_someone(**kwargs):
-    channel = kwargs.get('channel_name')
-    channel = u'#%s' % channel
+    channel = kwargs.get('channel_id')
     slappee = kwargs.get('text')
+    payload = {'icon_emoji': ':fish:', 'username': 'Troutslap!',
+               'channel': channel, 'unfurl_links': True}
     if slappee == '':
         message = '<http://i.imgur.com/R26mope.gifv>'
-        payload = {'text': message, 'unfurl_links': True, 'icon_emoji': ':fish:',
-        'username': 'Troutslap!', 'channel': channel}
+        payload.update({'text': message})
         r = requests.post(url, data=json.dumps(payload))
         return slack.response('')
     else:
-        slappee = slappee.replace('@','')
+        slappee = slappee.replace('@', '')
         slappee_id = user_id_from_name(slappee)
 
         slapper_id = kwargs.get('user_id')
         slapper = kwargs.get('user_name')
 
-        message = '<@%s|%s> slaps <@%s|%s> around a bit with a large trout!' % (
-           slapper_id, slapper, slappee_id, slappee)
-
-        payload = {'text': message, 'icon_emoji': ':fish:',
-        'username': 'Troutslap!', 'channel': channel}
+        message = ('<@{}|{}> slaps <@{}|{}> around a bit with a large trout!'
+                   .format(slapper_id, slapper, slappee_id, slappee))
+        payload.update({'text': message})
         r = requests.post(url, data=json.dumps(payload))
         return slack.response('')
+
 
 def user_id_from_name(user):
     slackapi.api_token = slacktoken
@@ -54,4 +80,3 @@ def user_id_from_name(user):
 
 # if __name__ == '__main__':
 #     app.run(debug=True, host='0.0.0.0', port=5000)
-
